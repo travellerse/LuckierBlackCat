3b23efa94b18130b500a19df6bd6e43faea4a30c
trave <trave@TRAVELLERS>
2024-11-16 15:06:16 +0800

    添加项目文件。

---
+++ LuckierBlackCat.cs
@@ -0,0 +1,10 @@
+﻿using BepInEx;
+
+namespace LuckierBlackCat
+{
+    [BepInPlugin("com.travellerse.plugins.LuckierBlackCat", "Luckier Black Cat", "0.1.0.0")]
+    [BepInProcess("Elin.exe")]
+    public class LuckierBlackCat : BaseUnityPlugin
+    {
+    }
+}
