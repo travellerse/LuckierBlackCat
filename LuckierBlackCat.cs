cd70c411d3174d3c77f6df205bc0e5413ce3a738
1394332633@qq.com <travellers0238@outlook.com>
2024-11-16 17:15:38 +0800

    实现取消lick距离限制

--- LuckierBlackCat.cs
+++ LuckierBlackCat.cs
@@ -1,4 +1,7 @@
 ﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.Collections.Generic;
 
 namespace LuckierBlackCat
 {
@@ -6,5 +9,59 @@ namespace LuckierBlackCat
     [BepInProcess("Elin.exe")]
     public class LuckierBlackCat : BaseUnityPlugin
     {
+        public static new ManualLogSource Logger;
+
+        public void Awake()
+        {
+            Logger = base.Logger;
+            Logger.LogInfo("Luckier Black Cat loaded!");
+            Harmony harmony = new Harmony("com.travellerse.plugins.LuckierBlackCat");
+            harmony.PatchAll();
+            Logger.LogInfo("Luckier Black Cat patched!");
+        }
+    }
+
+    [HarmonyPatch(typeof(ThingGen), "TryLickChest")]
+    public class ThingGenTryLickChestPatch
+    {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var codes = new List<CodeInstruction>(instructions);
+            for (int i = 0; i < codes.Count; i++)
+            {
+                if (codes[i].opcode == System.Reflection.Emit.OpCodes.Callvirt && codes[i].operand.ToString().Contains("Dist"))
+                {
+                    if (codes[i + 1].opcode == System.Reflection.Emit.OpCodes.Ldc_I4_3)
+                    {
+                        codes[i + 1].opcode = System.Reflection.Emit.OpCodes.Ldc_I4;
+                        codes[i + 1].operand = 1024;
+                        break;
+                    }
+                }
+            }
+            return codes;
+        }
+    }
+
+    [HarmonyPatch(typeof(Card), "SpawnLoot")]
+    public class SpawnLootPatch
+    {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var codes = new List<CodeInstruction>(instructions);
+            for (int i = 0; i < codes.Count; i++)
+            {
+                if (codes[i].opcode == System.Reflection.Emit.OpCodes.Callvirt && codes[i].operand.ToString().Contains("Dist"))
+                {
+                    if (codes[i + 1].opcode == System.Reflection.Emit.OpCodes.Ldc_I4_3)
+                    {
+                        codes[i + 1].opcode = System.Reflection.Emit.OpCodes.Ldc_I4;
+                        codes[i + 1].operand = 1024;
+                        break;
+                    }
+                }
+            }
+            return codes;
+        }
     }
 }
