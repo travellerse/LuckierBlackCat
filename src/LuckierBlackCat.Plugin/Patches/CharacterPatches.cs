0b9812ef71ed61374d1c5b77281fee63304cba36
travellerse <travellers0238@outlook.com>
2026-07-14 20:53:04 +0800

    refactor: route game events through core rules

--- src/LuckierBlackCat.Plugin/Patches/CharacterPatches.cs
+++ src/LuckierBlackCat.Plugin/Patches/CharacterPatches.cs
@@ -1,65 +1,54 @@
-using HarmonyLib;
-using LuckierBlackCat.Utils;
 using System;
+using HarmonyLib;
+using LuckierBlackCat.Core;
+using LuckierBlackCat.Utils;
 
 namespace LuckierBlackCat.Patches
 {
-    /// <summary>
-    /// Chara.Pick 方法的补丁类
-    /// 当玩家角色拾取装备时，自动触发黑猫的舔舐附魔效果
-    /// </summary>
     [HarmonyPatch(typeof(Chara), "Pick", new Type[] { typeof(Thing), typeof(bool), typeof(bool) })]
     public static class CharaPickPatch
     {
-        /// <summary>
-        /// 在 Pick 方法执行前调用，检查并触发舔舐附魔
-        /// </summary>
-        /// <param name="__instance">调用方法的角色实例</param>
-        /// <param name="t">要拾取的物品</param>
-        /// <param name="msg">是否显示消息</param>
-        /// <param name="tryStack">是否尝试堆叠</param>
-        /// <returns>是否继续执行原方法</returns>
-        private static bool Prefix(Chara __instance, ref Thing t, ref bool msg, ref bool tryStack)
+        private static void Prefix(Chara __instance, Thing t, out bool __state)
         {
-            // 只处理玩家角色的拾取行为
-            if (!__instance.IsPC) return true;
+            __state = t.GetRootCard() == __instance;
+        }
 
-            // 记录拾取行为的详细信息
-            Logger.LogInfo("Chara " + __instance.Name + "::Pick " + t.Name + " - Msg: " + msg + ", TryStack: " + tryStack);
-
-            // 检查物品是否符合舔舐条件
-            if (!BlackCatUtils.IsItemEligibleForLicking(t))
+        private static void Postfix(
+            Chara __instance,
+            Thing __result,
+            bool msg,
+            bool __state)
+        {
+            if (__result == null)
             {
-                return true;
+                return;
             }
 
-            // 寻找并执行舔舐附魔
-            BlackCatUtils.TryLickItem(t, msg);
+            bool resultOwnedByPlayer = __result.GetRootCard() == __instance;
+            if (!LickRules.ShouldLickAfterPickup(
+                    __instance.IsPC,
+                    __state,
+                    resultOwnedByPlayer,
+                    BlackCatUtils.ToItemFacts(__result)))
+            {
+                return;
+            }
 
-            return true; // 继续执行原方法
+            BlackCatUtils.TryLickItem(__result, msg);
         }
     }
 
-    /// <summary>
-    /// ActPray.TryPray 方法的补丁类
-    /// 当角色进行祈祷时，自动让黑猫对其身上符合条件的装备进行舔舐附魔
-    /// </summary>
     [HarmonyPatch(typeof(ActPray), "TryPray", new Type[] { typeof(Chara), typeof(bool) })]
     public static class ActPrayTryPrayPatch
     {
-        /// <summary>
-        /// 在 TryPray 方法执行后调用，为祈祷角色的装备进行舔舐附魔
-        /// </summary>
-        /// <param name="c">进行祈祷的角色</param>
-        /// <param name="passive">是否为被动祈祷</param>
-        private static void Postfix(Chara c, bool passive)
+        private static void Postfix(Chara c, bool __result)
         {
-            // 记录祈祷行为
-            Logger.LogInfo("ActPray::TryPray " + c.Name + " - Passive: " + passive);
+            if (!LickRules.ShouldProcessPrayer(c.IsPC, __result))
+            {
+                return;
+            }
 
-            // 使用BlackCatUtils的批量处理方法
             int lickCount = BlackCatUtils.LickAllEligibleItems(c, true);
-
             if (lickCount > 0)
             {
                 Logger.LogInfo("Prayer triggered licking for " + lickCount + " items on " + c.Name);
